312fb54e75062c3bccd13a1aa5e2df40fbc439c3
WaLLyBR <wlysn2@gmail.com>
2017-03-16 20:22:56 -0300

    Adição de "foreign key"

--- Mysql/Aula-2.sql
+++ Mysql/Aula-2.sql
@@ -1,22 +1,29 @@
-create database db_loja;
+create database db_pizzaria;
+use db_pizzaria;
 
-use db_loja;
+create table tb_pizzas(
+    piz_codigo int auto_increment primary key,
+    piz_sabor varchar(50) not null,
+    piz_tamanho varchar(2) not null,
+    piz_preco decimal(8,2) not null
 
-create table tb_marcas(
-	mar_codigo int auto_increment primary key,
-    mar_marca varchar(50) not null
 );
 
-create table tb_estados_conserv(
-	est_codigo int auto_increment primary key,
-    est_estado varchar(50) not null
+create table tb_clientes(
+    cli_codigo int auto_increment primary key,
+    cli_nome varchar(50) not null,
+    cli_mesa varchar(50) not null
+
 );
 
-create table tb_carros(
-	car_codigo int auto_increment primary key,
-    car_placa varchar(8) not null,
-    car_ano int not null,
-    car_est_codigo int not null references tb_estados_conserv(est_codigo),
-    car_mar_codigo int not null references tb_marcas(mar_codigo)
-    
+create table tb_pedidos(
+    ped_codigo int auto_increment primary key,
+    ped_data date not null,
+    ped_finalizado int not null,
+    ped_cli_codigo int, 
+    ped_piz_codigo int,
+    foreign key (ped_cli_codigo) references tb_clientes(cli_codigo),
+    foreign key (ped_piz_codigo) references tb_pizzas(piz_codigo)
+
+
 );
