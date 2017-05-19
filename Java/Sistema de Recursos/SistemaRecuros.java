1f3e4640a26892291312c03e6b7f077a63a7c7ee
WaLLyBR <wlysn2@gmail.com>
2017-05-19 11:46:37 -0300

    Adicionando e armazenando opções

--- Java/Sistema de Recursos/SistemaRecuros.java
+++ Java/Sistema de Recursos/SistemaRecuros.java
@@ -10,9 +10,18 @@ public class SistemaRecuros {
 
         switch (opcao) {
             case 1:
-                String nome = JOptionPane.showInputDialog(null, "Digite o nome do Engenheiro: ");
-
+                String nomeTemp = JOptionPane.showInputDialog(null, "Digite o nome do Engenheiro: ");
+                double salarioTemp = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o salario base: "));
+                String departamentoTemp = JOptionPane.showInputDialog(null, "Digite o Departamento: ");
+                String creaTemp = JOptionPane.showInputDialog(null, "Digite o crea: ");
+                String codigoTemp = JOptionPane.showInputDialog(null, "Digite o codigo: ");
+                
+                Engenheiro e = new Engenheiro(departamentoTemp, creaTemp, nomeTemp, codigoTemp,salarioTemp);
+                
+                JOptionPane.showMessageDialog(null,"REGISTRO DO ENGENHEIRO COMPLETO: \n\nNome: "+ e.getNome()+"\nCodigo: "+e.getCodigo()+"\nSalario: "+e.getSalarioEngenheiro()+"\nDepartamento: "+e.getDepartamento()+"\ncrea: "+e.getCrea());
         }
 
     }
+
+  
 }
